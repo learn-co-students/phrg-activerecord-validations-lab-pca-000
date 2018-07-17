8d88b66cb40b23291f59e3c2d88ff2e285444402
Nick Wohnhas <nicholas.wohnhas@powerhrg.com>
2018-07-17 10:27:37 -0400

    validation practice

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,27 @@
 # frozen_string_literal: true
 
 class Post < ActiveRecord::Base
+  include ActiveModel::Validations
+  validates :title, presence: true
+  validates :content, length: { minimum: 250 }
+  validates :summary, length: { maximum: 250 }
+  validates :category, inclusion: { in: %w[Non-Fiction Fiction] }
+
+  validate :click_bait
+
+private
+
+  def click_bait
+    click_bait?
+  end
+
+  def click_bait?
+    unless title.present? &&
+           (title.include?("Won't Believe") ||
+           title.include?("Secret") ||
+           title.include?("Top [number]") ||
+           title.include?("Guess"))
+      errors.add(:base, "Not valid")
+    end
+  end
 end
