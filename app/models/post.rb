a58e7f8118ae504e4771c58bc35098b98dc298a6
Katie Weber <k.weber0893@gmail.com>
2018-07-19 11:47:22 -0400

    Completed Validations w Rubocop

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,19 +4,18 @@ class Post < ActiveRecord::Base
   validates :title, presence: true
   validates :content, length: { minimum: 250 }
   validates :summary, length: { maximum: 250 }
-  validates :category, inclusion: { in: %w( Fiction Non-Fiction) }
-  validate :is_clickbait?
+  validates :category, inclusion: { in: %w[Fiction Non-Fiction] }
+  validate :clickbait?
 
 private
 
-  def is_clickbait?
+  def clickbait?
     @clickbait =
-    [/Won't Believe/i,
-    /Secret/i,
-    /Top [0-9]*/i,
-    /Guess/i]
-    if @clickbait.none? { |bait| bait.match title }
-      errors.add(:title, "Title must be clickbait")
-    end
+      [/Won't Believe/i,
+       /Secret/i,
+       /Top [0-9]*/i,
+       /Guess/i]
+    return unless @clickbait.none? { |bait| bait.match title }
+    errors.add(:title, "Title must be clickbait")
   end
 end
