082717571c4d3d3263003707b76797d42907b25e
Katie Weber <k.weber0893@gmail.com>
2018-07-19 11:42:33 -0400

    Making commit before running rubocop

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,22 @@
 # frozen_string_literal: true
 
 class Post < ActiveRecord::Base
+  validates :title, presence: true
+  validates :content, length: { minimum: 250 }
+  validates :summary, length: { maximum: 250 }
+  validates :category, inclusion: { in: %w( Fiction Non-Fiction) }
+  validate :is_clickbait?
+
+private
+
+  def is_clickbait?
+    @clickbait =
+    [/Won't Believe/i,
+    /Secret/i,
+    /Top [0-9]*/i,
+    /Guess/i]
+    if @clickbait.none? { |bait| bait.match title }
+      errors.add(:title, "Title must be clickbait")
+    end
+  end
 end
