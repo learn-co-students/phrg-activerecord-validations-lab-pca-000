100b9622e948675971a45fa5ee0661fe7684b46e
GregBenn <gbenn07@gmail.com>
2018-08-06 14:59:54 -0400

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,31 @@
 # frozen_string_literal: true
 
 class Post < ActiveRecord::Base
+  validates :title, presence: true
+  validates :content, length: { minimum: 250 }
+  validates :summary, length: { maximum: 250 }
+  validates :category, inclusion: { in: %w[Fiction Non-Fiction] }
+  validate :not_clickbait
+
+  def not_clickbait
+    return if title =~ /Won't Believe|Secret|Top \d|Guess/
+    errors.add(:title, "is clickbait")
+  end
 end
+
+# def not_clickbait
+#   if !self.title.match(/Won't Believe|Secret|Top \d|Guess/)
+#     errors.add(:title, "is clickbait")
+#   end
+# end
+
+# def not_clickbait
+#   if !/Won't Believe|Secret|Top \d|Guess/.match(title)
+#     errors.add(:title, "is clickbait")
+#   end
+# end
+
+# def not_clickbait
+#   unless title =~ /Won't Believe|Secret|Top \d|Guess/
+#   errors.add(:title, "is clickbait")
+# end
