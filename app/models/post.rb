3aeed89933db415096b80ebeb1f9844712b039b6
Ezz Abousaif <ezzeldin.abousaif@powerhrg.com>
2019-05-16 12:58:11 -0400

    Validations lab

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,21 @@
 # frozen_string_literal: true
 
 class Post < ActiveRecord::Base
+  validates :title, presence: true
+  validates(:content, length: { minimum: 250 })
+  validates(:summary, length: { maximum: 250 })
+  validates :category, inclusion: { in: %w[Fiction Non-Fiction] }
+  validate :clickbait
+
+  def clickbait
+    errors.add(:title, "Post needs a clickbait-y title") unless clickbait?
+  end
+
+  def clickbait?
+    !title.nil? && (
+      title.include?("Won't Believe") ||
+      title.include?("Secret") ||
+      title.include?("Top") ||
+      title.include?("Guess"))
+  end
 end
