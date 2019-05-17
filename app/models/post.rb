26804520fe8b236b8cd408d1791a3eca0e0bc3a5
KFad <kfadigan@yahoo.com>
2019-05-17 08:36:16 -0400

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,10 @@
 # frozen_string_literal: true
 
 class Post < ActiveRecord::Base
+  validates :title, presence: true
+  validates :content, length: { minimum: 150 }
+  validates :summary, length: { maximum: 152 }
+  validates :category, inclusion: { in: %w[Fiction] }
+  include ActiveModel::Validations
+  validates_with MyValidator
 end
