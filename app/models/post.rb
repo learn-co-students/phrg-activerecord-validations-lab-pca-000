70bdac9fa488299e17b60bc8a961bc0ecb12b922
Gillian Wenhold <gwenhold@mac.com>
2019-07-15 21:51:57 -0400

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,14 @@
 # frozen_string_literal: true
 
 class Post < ActiveRecord::Base
+  validates :title, presence: true
+  validates :content, length: { minimum: 250 }
+  validates :summary, length: { maximum: 250 }
+  validates :category, inclusion: { in: %w[Fiction Non-Fiction] }
+  validate :clickbait
+
+  def clickbait
+    words = [/Won't Believe/, /Secret/, /Top [0-9]*/, /Guess/]
+    errors.add(:title, "Add Clickbait") unless words.any? { |w| w.match title }
+  end
 end
